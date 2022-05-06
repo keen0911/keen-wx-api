7c5e2e2f5036ee1e3babd150fd71efb5dc2c58c9
keen <2409306823@qq.com>
2022-05-06 23:54:20 +0800

    生成mapper文件

--- src/main/java/com/example/emos/wx/EmosWxApiApplication.java
+++ src/main/java/com/example/emos/wx/EmosWxApiApplication.java
@@ -1,4 +1,4 @@
-package com.example.emos.wx.api;
+package com.example.emos.wx;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
