f9f3e6e8477e42f523ea3bd918e6d7c80f5a9dc2
keen <2409306823@qq.com>
2022-05-06 21:47:36 +0800

    Initial commit

---
+++ src/test/java/com/example/emos/wx/api/EmosWxApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.emos.wx.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmosWxApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
