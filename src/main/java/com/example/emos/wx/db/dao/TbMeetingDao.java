af719b363acdb317c4aa34dd6c1cacbb9fd6f45d
keen <2409306823@qq.com>
2022-05-06 23:56:01 +0800

    生成mapper文件

---
+++ src/main/java/com/example/emos/wx/db/dao/TbMeetingDao.java
@@ -0,0 +1,19 @@
+package com.example.emos.wx.db.dao;
+
+import com.example.emos.wx.db.pojo.TbMeeting;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface TbMeetingDao {
+    int deleteByPrimaryKey(Long id);
+
+    int insert(TbMeeting record);
+
+    int insertSelective(TbMeeting record);
+
+    TbMeeting selectByPrimaryKey(Long id);
+
+    int updateByPrimaryKeySelective(TbMeeting record);
+
+    int updateByPrimaryKey(TbMeeting record);
+}
