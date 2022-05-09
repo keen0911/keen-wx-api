7c5be6eb5c1c7688981f3fcc45970022026eb60d
x6plus <819475305@qq.com>
2022-05-09 16:08:50 +0800

    认证与授权结束

---
+++ src/main/java/com/example/emos/wx/controller/TestController.java
@@ -0,0 +1,25 @@
+package com.example.emos.wx.controller;
+
+import com.example.emos.wx.common.util.R;
+
+import com.example.emos.wx.controller.form.TestSayHelloForm;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+
+@RestController
+@RequestMapping("/test")
+@Api("测试Web接口")
+public class TestController {
+    @PostMapping("/sayHello")
+    @ApiOperation("最简单的测试方法")
+    public R sayHello(@Valid @RequestBody TestSayHelloForm form) {
+        return R.ok().put("message", "Hello,"+form.getName());
+    }
+
+
+}
--- src/main/java/com/example/emos/wx/exception/EmosException.java
+++ src/main/java/com/example/emos/wx/exception/EmosException.java
@@ -11,4 +11,20 @@ private int code = 500;
         super(msg);
         this.msg = msg;
     }
+    public EmosException(String msg, Throwable e) {
+        super(msg, e);
+        this.msg = msg;
+    }
+
+    public EmosException(String msg, int code) {
+        super(msg);
+        this.msg = msg;
+        this.code = code;
+    }
+
+    public EmosException(String msg, int code, Throwable e) {
+        super(msg, e);
+        this.msg = msg;
+        this.code = code;
+    }
 }
