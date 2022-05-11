0d2e0e4ddab8858f0598e89146e137a4ff787c18
keen <2409306823@qq.com>
2022-05-11 22:41:42 +0800

    解决注册运行bug-0511

--- src/main/java/com/example/emos/wx/controller/UserController.java
+++ src/main/java/com/example/emos/wx/controller/UserController.java
@@ -6,9 +6,11 @@ import com.example.emos.wx.config.shiro.JwtUtil;
 import com.example.emos.wx.controller.form.RegisterForm;
 import com.example.emos.wx.service.UserService;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -45,7 +47,8 @@ public class UserController {
     private void saveCacheToken(String token,int userId){
         redisTemplate.opsForValue().set(token,userId+"",cacheExpire, TimeUnit.DAYS);
     }
-
+    @PostMapping("/register")
+    @ApiOperation("注册用户")
     public R register(@Valid @RequestBody RegisterForm form){
         int id=userService.registerUser(form.getRegisterCode(),form.getCode(),form.getNickname(),form.getPhoto());
         String token=jwtUtil.createToken(id);
